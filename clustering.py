07dd66d4b45a4d2d0aa6cf6e95916957d8af0ea6
Paul Konstantin Gerke <paul@amd6300fx.(none)>
2013-04-01 22:58:00 +0200

    Added: Experimental clusters (not workin :P) and more standard pair-wise hierachical cluster algorithms

--- clustering.py
+++ clustering.py
@@ -5,8 +5,78 @@ from pylab import *
 def euclidean(x, y):
   return sqrt(sum((array(x) - array(y))**2))
 
-def PKGCluster(data, distanceFunc=euclidean):
+def MeanSquareLengthLimitTracer(data, distanceFunc=lambda x, y: euclidean(x, y)**1):
   data = [array(x) for x in data]
+
+  vIsNone = vectorize(lambda x: x is None)
+
+  distances = zeros((len(data), len(data))) - 1
+  for i in range(0, len(data)):
+    for j in range(0, len(data)):
+      if i != j:
+        if i > j:
+          distances[i, j] = distances[j, i]
+        else:
+          distances[i, j] = distanceFunc(data[i], data[j])
+          
+  distances_distribution = {}
+  distances_distribution["mean"] = mean(distances[distances >= 0])
+  distances_distribution["stddev"] = std(distances[(distances >= 0) * (distances < distances_distribution["mean"])])
+  
+  cluster = array([None] * len(data))
+  clusterIndex = 0
+
+  indexLookup = array(range(len(data)))
+  limit = distances_distribution["mean"] - distances_distribution["stddev"]
+  short_distances = (distances < limit)
+
+  print(limit)
+  f = figure()  
+  p = f.add_subplot(1, 1, 1)
+  p.hold(True)
+  p.hist(distances.flatten())
+  f.show()
+  show()
+  
+  while vIsNone(cluster).any():
+    seed = indexLookup[short_distances.max(0)].min(0)
+    searchStack = [seed]
+    
+    # Search all elements from one cluster
+    clusterElements = []
+    while len(searchStack) > 0:
+      e = searchStack.pop(0)
+      
+      subElements = short_distances[e,:]
+      if not subElements.any():
+        continue
+      
+      clusterElements.append(e)
+      searchStack += list(indexLookup[subElements])
+      
+      short_distances[:,e] = False
+      short_distances[e,:] = False
+      
+    # Write labels for elements and continue
+    vIsInElements = vectorize(lambda x: x in clusterElements);
+    cluster[vIsInElements(indexLookup)] = clusterIndex
+    clusterIndex += 1
+    
+  return cluster
+
+def PKGCluster(data, distanceFunc=euclidean, labels=None):
+  """
+  Has problems - 3 items are ALWAYS clusterd into ONE cluster even if 
+  a cluster of two and one with a single element would be more optimal. 
+  
+  Example:
+    x x                           x
+    
+  """
+  data = [array(x) for x in data]
+  
+  if labels != None:
+    labels = array(labels)
   
   distances = zeros((len(data), len(data)))
   for i in range(0, len(data)):
@@ -39,14 +109,50 @@ def PKGCluster(data, distanceFunc=euclidean):
         cluster[i] = cluster[j]
       else:
         cluster[j] = cluster[i]
-        
-    clusterFilter[i] = False
-    clusterFilter[j] = False
+
+    if labels is None:
+      clusterFilter[i] = False
+      clusterFilter[j] = False
+    else:
+      f = labels == labels[i]
+      cluster[f] = cluster[i]
+      clusterFilter[f] = False
+      
+      f = labels == labels[j]
+      cluster[f] = cluster[j]
+      clusterFilter[f] = False
     
     valid *= -outer(-clusterFilter, -clusterFilter)
   
   return cluster
   
+def pairer(data, distanceFunction=euclidean):
+  """
+    Note: Returns fully clustered tree in form of lists
+  """
+  data = [array(x) for x in data]
+  
+  def clusterDistance(c1, c2):
+    if type(c1) is list:
+      return min([clusterDistance(x, c2) for x in c1])
+    elif type(c2) is list:
+      return clusterDistance(c2, c1)
+    else:
+      return distanceFunction(data[c1], data[c2])
+      
+  indexes = list(range(len(data)))
+  while len(indexes) > 2:
+    distances = []
+    for i in range(len(indexes)):
+      for j in range(i + 1, len(indexes)):
+        distances.append((i, j, clusterDistance(indexes[i], indexes[j])))
+    i, j, minDistance = min(distances, key=lambda x: x[2])
+    
+    # Unify pair
+    indexes.append([indexes[i], indexes[j]])
+    del indexes[j];
+    del indexes[i];
+  return indexes
 
 if __name__ == '__main__':
   targetFigure = figure()
@@ -58,5 +164,6 @@ if __name__ == '__main__':
   targetFigure.show()
   
   points = targetFigure.ginput(0)
+  ioff()
   
-  print(PKGCluster(points))
+  print(pairer(points))
