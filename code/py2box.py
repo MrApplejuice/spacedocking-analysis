206e6e862ed986915c62342586cb35e1a798e679
Paul Konstantin Gerke <paul@amd6300fx.(none)>
2013-04-10 01:14:11 +0200

    Fixed: py#box.py scripts to work from any directory

--- code/py2box.py
+++ code/py2box.py
@@ -3,7 +3,8 @@
 # Loads the all files for interactive mode in python 2
 
 import sys
-sys.path += ["py2", "universal"]
+import os
+sys.path += [os.path.join(os.path.realpath("."), os.path.dirname(sys.argv[0]), p) for p in ["py2", "universal"]]
 
 from readdata import *
 from clustering import *
--- code/py3box.py
+++ code/py3box.py
@@ -3,7 +3,8 @@
 # Loads the all files for interactive mode in python 2
 
 import sys
-sys.path += ["py3", "universal"]
+import os
+sys.path += [os.path.join(os.path.realpath("."), os.path.dirname(sys.argv[0]), p) for p in ["py3", "universal"]]
 
 from readdata import *
 from clustering import *
