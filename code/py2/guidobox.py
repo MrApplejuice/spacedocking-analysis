88eac76b1ad1f6584e3d8c178c979e092a0f324a
guidoAI <guido.de.croon@gmail.com>
2013-04-13 12:08:37 +0200

    match features in samples against an entire image directory

--- code/py2/guidobox.py
+++ code/py2/guidobox.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+from readdata import *
+from clustering import *
+import os
 import pdb
 import cv2
 import numpy as np;
@@ -162,14 +165,17 @@ def matchTwoImages(test_dir, image_name1, image_name2, NN_THRESHOLD = 0.9):
 	#cv2.namedWindow("img2", cv2.cv.CV_WINDOW_NORMAL)
 	#cv2.imshow('img2',img2)
 
-def extractSURFfeaturesFromImage(image_name):
+def extractSURFfeaturesFromImage(image_name, IM_RESIZE=False, W=640, H=360):
 	im2 = cv2.imread(image_name);
 	
 	#cv2.namedWindow("img", cv2.cv.CV_WINDOW_NORMAL)
 	#cv2.imshow('img',im2)
 	
 	im = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
-	im = cv2.resize(im, (320, 240))
+	
+	if(IM_RESIZE):
+		im = cv2.resize(im, (W, H));
+	
 	#im = cv2.resize(im, (im.shape[1] / 2, im.shape[0] / 2))
 	
 	surfDetector = cv2.SURF();
@@ -186,98 +192,123 @@ def extractSURFfeaturesFromImage(image_name):
 	(keypoints, descriptors) = surfDescriptorExtractor.compute(im,keypoints)
 	return (keypoints, descriptors, im2, im);
 
-def testExperimentalSetup(test_dir, image_name, data_name):
+
+
+def testExperimentalSetup(test_dir="../data_GDC", target_name="video_CocaCola", data_name="output_GDC.txt", VIDEO = True, histogram_sizes = False):
 	
-	# the data will serve as a database for matching with the image:
+	# the data will serve as a database for matching with the image(s):
 
 	# read the data:
 	result = loadData(test_dir + "/" + data_name);
-
-	# extract surf features from the image:
-	im2 = cv2.imread(test_dir + "/" + image_name);
-	im = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
-	im = cv2.resize(im, (160, 120))
-	# im = cv2.resize(im, (im.shape[1] / 4, im.shape[0] / 4))
-	
-	surfDetector = cv2.SURF();
-	surfDetector.hessianThreshold = 2500;
-	surfDetector.nOctaves = 4;
-	surfDetector.nOctaveLayers = 2;
-	mask = np.ones(im.shape, dtype=np.uint8)
-	keypoints = surfDetector.detect(im, mask);
-	
-	#surfDetector = cv2.FeatureDetector_create("SURF")
-	#keypoints = surfDetector.detect(im)
-	
-	surfDescriptorExtractor = cv2.DescriptorExtractor_create("SURF")
-	(keypoints, descriptors) = surfDescriptorExtractor.compute(im,keypoints)
-	n_features_image = len(descriptors);
-	print 'number of features in image: %d' % n_features_image
-	descriptor_magnitudes_image = np.array([0.0] * n_features_image);
-	for fi in range(n_features_image):
-		magn = np.linalg.norm(descriptors[fi]);
-		#print 'fi = %d, magn = %f' % (fi, magn);
-		descriptor_magnitudes_image[fi] = magn;
-	
-	#pl.figure(facecolor='white', edgecolor='white');
-	#pl.hist(descriptor_magnitudes_image, 30);
-	#pl.title('descriptor magnitudes image');
 
 	# definition of a match:
 	NN_THRESHOLD = 0.75;
 	
 	# iterate over the data:
 	n_samples = len(result);
-	sam = 0;
-	for sample in result:
-		# number of frames in sample:
-		n_frames = len(sample['frames']);
-		
-		print 'Sample %d' % sam;
-		
-		for fr in range(n_frames):
-		
-			n_matches = 0;
-		
-			# define current frame:
-			frame = sample['frames'][fr];
-			n_features_frame = len(frame['features']['features']);
-			print 'number of features in frame %d = %d' % (fr, n_features_frame); 
-			
-			# compare descriptor magnitude with the ones from the image:
-			descriptor_magnitudes_frame = np.array([0.0] * n_features_frame);
-			for ff in range(n_features_frame):
-				descriptor_magnitudes_frame[ff] = np.linalg.norm(frame['features']['features'][ff]['descriptor']);
-				
-			# plot histograms descriptor magnitudes image / frame for comparison:
-			#pl.figure(facecolor='white', edgecolor='white');
-			#pl.hist([np.asarray(descriptor_magnitudes_image), np.asarray(descriptor_magnitudes_frame)], 30, color=[(0.2,0.2,0.2), (1.0,1.0,1.0)]);
-			# pl.hist(descriptor_magnitudes_frame, 30);
-			
-			closest_distances = np.array([0.0] * n_features_image);
-			for ft1 in range(n_features_image):
-			
-				step = int(round(n_features_image / 10));
-				if(ft1 % step == 0):
-					print '.',
 
-				# determine the distances to the features in the frame:
-				distances = np.array([0.0] * n_features_frame);
-				for ft2 in range(n_features_frame):
-					#pdb.set_trace();
-					distances[ft2] = np.linalg.norm(np.array(descriptors[ft1]) - np.array(frame['features']['features'][ft2]['descriptor']));
-					# print 'distances[%d] = %f' % (ft2, distances[ft2]);
-			
-				# sort the distances:
-				sindices = np.argsort(distances);
-				
-				# store the closest distance:
-				closest_distances[ft1] = distances[sindices[0]];
-				
-				# the second nearest neighbor has to be sufficiently far for a match:
-				if(distances[sindices[0]] / distances[sindices[1]] < NN_THRESHOLD):
-					n_matches += 1;
-			
-			print '\nFrame %d, number of matches = %d, average closest match (dist in feature space) = %f\n' % (fr, n_matches, np.mean(closest_distances));
+
+	if(not(VIDEO)):
+		# target is a single image:
+		image_names = [test_dir + "/" + target_name];
+	else:
+		# get image names from directory:
+		image_names = os.listdir(test_dir + "/" + target_name);
+	
+	# number of images:
+	n_images = len(image_names);
+	
+	Distances = np.zeros([n_images, n_samples]);
+	Matches = np.zeros([n_images, n_samples]);
+	
+	imn = 0;
+	for iname in image_names:
+	
+		print 'Image %d' % imn
 		
-		sam += 1;
+		if(VIDEO):
+			image_name = test_dir + "/" + "/" + target_name + "/" + iname;
+		else:
+			image_name = test_dir + "/" + "/" + iname;
+			
+		(keypoints, descriptors, im2, im) = extractSURFfeaturesFromImage(image_name);
+		
+		n_features_image = len(descriptors);
+		# print 'number of features in image: %d' % n_features_image
+		if(histogram_sizes):
+			descriptor_magnitudes_image = np.array([0.0] * n_features_image);
+			for fi in range(n_features_image):
+				magn = np.linalg.norm(descriptors[fi]);
+				#print 'fi = %d, magn = %f' % (fi, magn);
+				descriptor_magnitudes_image[fi] = magn;
+		
+		sam = 0;
+		for sample in result:
+			# number of frames in sample:
+			n_frames = len(sample['frames']);
+			
+			print 'Sample %d' % sam;
+			
+			for fr in range(n_frames):
+			
+				n_matches = 0;
+			
+				# define current frame:
+				frame = sample['frames'][fr];
+				n_features_frame = len(frame['features']['features']);
+				# print 'number of features in frame %d = %d' % (fr, n_features_frame); 
+
+				if(histogram_sizes):
+					# compare descriptor magnitude with the ones from the image:
+					descriptor_magnitudes_frame = np.array([0.0] * n_features_frame);
+					for ff in range(n_features_frame):
+						descriptor_magnitudes_frame[ff] = np.linalg.norm(frame['features']['features'][ff]['descriptor']);
+					
+					# plot histograms descriptor magnitudes image / frame for comparison:
+					pl.figure(facecolor='white', edgecolor='white');
+					pl.hist([np.asarray(descriptor_magnitudes_image), np.asarray(descriptor_magnitudes_frame)], 30, color=[(0.2,0.2,0.2), (1.0,1.0,1.0)]);
+				
+				closest_distances = np.array([0.0] * n_features_image);
+				for ft1 in range(n_features_image):
+				
+					step = int(round(n_features_image / 10));
+					#if(ft1 % step == 0):
+					#	print '.',
+
+					# determine the distances to the features in the frame:
+					distances = np.array([0.0] * n_features_frame);
+					for ft2 in range(n_features_frame):
+						#pdb.set_trace();
+						distances[ft2] = np.linalg.norm(np.array(descriptors[ft1]) - np.array(frame['features']['features'][ft2]['descriptor']));
+						# print 'distances[%d] = %f' % (ft2, distances[ft2]);
+				
+					# sort the distances:
+					sindices = np.argsort(distances);
+					
+					# store the closest distance:
+					closest_distances[ft1] = distances[sindices[0]];
+					
+					# the second nearest neighbor has to be sufficiently far for a match:
+					if(distances[sindices[0]] / distances[sindices[1]] < NN_THRESHOLD):
+						n_matches += 1;
+				
+				# gather statistics:
+				#pdb.set_trace();
+				print 'im = %d, sam = %d' % (imn, sam)
+				Matches[imn][sam] += n_matches;
+				Distances[imn][sam] += np.mean(closest_distances);
+				
+				print '\nFrame %d, number of matches = %d, average closest match (dist in feature space) = %f\n' % (fr, n_matches, np.mean(closest_distances));
+			
+			# to keep the average distance:
+			Distances[imn][sam] /= n_frames;
+			
+			sam += 1;
+		imn += 1;
+	
+	pdb.set_trace();
+	colors = [(1,0,0), (0,1,0), (0,0,1)];
+	pl.figure();
+	pl.hold = True;
+	for sam in range(n_samples):
+		pl.plot(Distances[:][sam], color=colors[mod(sam, 3)]);
