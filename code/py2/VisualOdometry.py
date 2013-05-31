5a0280e3399f6d89772362e556310b725a6e9e0f
Guido de Croon <guido@guido-TUD.(none)>
2013-06-01 00:05:57 +0200

    generate points and project them into two cameras

--- code/py2/VisualOdometry.py
+++ code/py2/VisualOdometry.py
@@ -6,15 +6,18 @@ def testVisualOdometry(n_points=30):
 
 	# location camera 1:
 	l1 = np.zeros([3,1]);
+	R1 = np.eye(3);
+	rvec1 = cv2.Rodrigues(R1);
+	rvec1 = rvec1[0];
 
 	# translation vector
-	t = np.random.rand(3,1);
+	t = np.random.rand(3,1)* 0.001;
 	l2 = l1 + t;
 
 	# Rotation matrix:
-	phi = 0.1*(np.random.random(1)*2-1) * numpy.pi;
-	theta = 0.1*(np.random.random(1)*2-1) * numpy.pi;
-	psi = 0.1*(np.random.random(1)*2-1) * numpy.pi;
+	phi = 0.001*(np.random.random(1)*2-1) * np.pi;
+	theta = 0.001*(np.random.random(1)*2-1) * np.pi;
+	psi = 0.001*(np.random.random(1)*2-1) * np.pi;
 
 	R_phi = np.zeros([3,3]);
 	R_phi[0,0] = 1;
@@ -31,21 +34,25 @@ def testVisualOdometry(n_points=30):
 	R_theta[2,2] = np.cos(phi);
 
 	R_psi = np.zeros([3,3]);
-	R_psi[1,1] = np.cos(phi);
-	R_psi[1,2] = np.sin(phi);
-	R_psi[2,1] = -np.sin(phi);
-	R_psi[2,2] = np.cos(phi);
+	R_psi[0,0] = np.cos(psi);
+	R_psi[0,1] = np.sin(psi);
+	R_psi[1,0] = -np.sin(psi);
+	R_psi[1,1] = np.cos(psi);
+	R_psi[2,2] = 1;
 
-	R = np.dot(R_psi, np.dot(R_theta, R_phi));
+	R2 = np.dot(R_psi, np.dot(R_theta, R_phi));
+
+	rvec2 = cv2.Rodrigues(R2);
+	rvec2 = rvec2[0];
 
 	# create X, Y, Z points:
 	size = 3;
 	distance = 4;
 	transl = np.zeros([1,3]);
-	transl[2] = distance;
+	transl[0,2] = distance;
 	points_world = np.zeros([n_points, 3]);
 	for p in range(n_points):
-		points_world[p, :] = size * (np.rand(1,3)*2-np.ones([1,3])) + transl;
+		points_world[p, :] = size * (np.random.rand(1,3)*2-np.ones([1,3])) + transl;
 
 	# camera calibration matrix:
 	K = np.zeros([3,3]);
@@ -53,12 +60,18 @@ def testVisualOdometry(n_points=30):
 	K[1,1] = 240.0;
 	K[2,2] = 1.0;
 	
-	
+	distCoeffs = np.zeros([4]); # no clue what this means
 
+	pdb.set_trace();
+	result = cv2.projectPoints(points_world, rvec1, np.array([0.0]*3), K, distCoeffs);	
+	image_points1 = result[0];
+
+	result = cv2.projectPoints(points_world, rvec2, t, K, distCoeffs);
+	image_points2 = result[0];
 
 
 def determineEssentialMatrix():
-
+	print 'a';
 
 #cvFindFundamentalMat(const CvMat* points1, const CvMat* points2, CvMat* fundamentalMatrix, int method=CV_FM_RANSAC, double param1=1., double param2=0.99, CvMat* status=NULL)
 #cvDecomposeProjectionMatrix(const CvMat *projMatrix, CvMat *cameraMatrix, CvMat *rotMatrix, CvMat *transVect, CvMat *rotMatrX=NULL, CvMat *rotMatrY=NULL, CvMat *rotMatrZ=NULL, CvPoint3D64f *eulerAngles=NULL)
