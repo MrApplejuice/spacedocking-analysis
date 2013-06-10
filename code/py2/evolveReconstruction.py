5e6ede5fd273a64fe48c17498cecde044bf48bcb
Guido de Croon <guido@guido-TUD.(none)>
2013-06-11 00:20:54 +0200

    added evolution of solution - still have to debug

---
+++ code/py2/evolveReconstruction.py
@@ -0,0 +1,142 @@
+from PyGMO import *
+from pickle import *
+from reconstructionProblem import *
+import numpy as np
+
+def evolveReconstruction(filename = 'test', n_views=2, n_points=100, IPs=[], t_scale = 3.0, world_scale=10.0, K = [], x = []):
+	
+	sd = 1234;
+	prob = reconstructionProblem(n_views, n_points, IPs, t_scale, world_scale, K);
+	n_genes = prob.getGenomeSize();
+	print 'n_genes = %d' % n_genes
+	
+	# all values are scaled afterward:
+	prob.lb = [-1.0] * n_genes;
+	prob.ub = [1.0] * n_genes;
+
+	# evolutions, generations and individuals
+	n_evolutions = 20;
+	n_generations = 50;
+	n_individuals = 50;
+
+	if(len(x) == 0):
+		# prior on camera matrices and points:
+		for v in range(n_views-1):
+			x = x + [0.0] * 6;		
+		for p in range(n_points):
+			x = x + [0.0, 0.5, 0.0];
+
+
+	print 'Creating algorithms'
+	algo_list = []
+	# algo_list.append(algorithm.monte_carlo(iter = n_generations))
+	# algo_list.append(algorithm.py_cmaes(gen = n_generations))
+	# algo_list.append(algorithm.de(gen = n_generations))
+	
+	#algo_list.append(algorithm.pso_gen(gen = n_generations, vcoeff = 0.2));
+	#algo_list.append(algorithm.pso_gen(gen = n_generations, vcoeff = 0.1));
+	# algo_list.append(algorithm.pso_gen(gen = n_generations, vcoeff = 0.2));
+	# algo_list.append(algorithm.pso_gen(gen = n_generations, vcoeff = 0.2));
+	#algo_list.append(algorithm.pso_gen(gen = n_generations, variant=3))
+	#algo_list.append(algorithm.pso_gen(gen = n_generations, variant=4))
+	#algo_list.append(algorithm.pso_gen(gen = n_generations, variant=5))
+	#algo_list.append(algorithm.pso_gen(gen = n_generations, variant=6))
+	#algo_list.append(algorithm.pso_gen(gen = n_generations, variant=5, neighb_type=1))
+	#algo_list.append(algorithm.pso_gen(gen = n_generations, variant=5, neighb_type=3))
+	#algo_list.append(algorithm.pso_gen(gen = n_generations, variant=5, neighb_type=4))
+	#algo_list.append(algorithm.pso_gen(gen = n_generations, variant=6, neighb_type=1))
+	#algo_list.append(algorithm.pso_gen(gen = n_generations, variant=6, neighb_type=3))
+	#algo_list.append(algorithm.pso_gen(gen = n_generations, variant=6, neighb_type=4))
+	
+	algo_list.append(algorithm.sga(gen = n_generations, cr = 0, m = 0.05))
+	algo_list.append(algorithm.sga(gen = n_generations, cr = 0.05, m = 0.05))
+	algo_list.append(algorithm.sga(gen = n_generations, cr = 0.05, m = 0.01))
+	algo_list.append(algorithm.sga(gen = n_generations, cr = 0.1, m = 0.03))
+	algo_list.append(algorithm.sga(gen = n_generations, cr = 0.5, m = 0.5))
+	algo_list.append(algorithm.sga(gen = n_generations, cr = 0.005, m = 0.001))
+	
+	#algo_list.append(algorithm.sga(gen = n_generations, cr = 0.1, m = 0.03, selection = algorithm._algorithm._selection_type.BEST20))
+	#algo_list.append(algorithm.sga(gen = n_generations, cr = 0.1, m = 0.03, selection = algorithm._algorithm._selection_type.BEST20))
+	#algo_list.append(algorithm.sga(gen = n_generations, cr = 0.1, m = 0.03, selection = algorithm._algorithm._selection_type.BEST20))
+	
+	#algo_list.append(algorithm.sga(gen = n_generations, cr = 0.1, m = 0.03, selection = algorithm._algorithm._selection_type.BEST20))
+	#algo_list.append(algorithm.sga(gen = n_generations, cr = 0.1, m = 0.03, selection = algorithm._algorithm._selection_type.BEST20))
+
+	topo = topology.ring()
+
+	print 'Creating archipelago'
+	archi = archipelago(topology = topo)
+	# len(algo_list)
+	
+	n_islands = len(algo_list);
+	
+
+
+	for i in range(0, n_islands):
+			print 'Island %d' % i
+			if(len(x) == 0):
+				archi.push_back(island(algo_list[i], prob, n_individuals))
+			else:
+				print 'starting with prior!'
+				pop = population(prob);
+				for ind in range(n_individuals):
+					pop.push_back(x);
+				archi.push_back(island(algo_list[i], pop))
+			print 'Done with push back'
+
+	
+	
+	avg_fits = np.zeros([n_islands,1]);
+	
+	for evs in range(0,n_evolutions):
+		print '****************'
+		print 'Evolving step %d' % evs
+		print '****************'
+		archi.evolve(1);
+		archi.join()
+		
+		# save results until now
+		# show the results for this step:
+		i = 0
+		isl_num = 1
+		best_fit = 1E4;
+		best_ind = 0;
+		for isl in archi:
+			f = open('genome_'+filename+'_'+str(isl_num), "a")
+			f2 = open('fitness'+filename+'_'+str(isl_num), "a")	
+			champ = isl.population.champion;
+			isl_num = isl_num + 1
+			print 'Isl. %d' % i,
+			print 'fit %3.3f ' % champ.f,
+			if(champ.f < best_fit):
+				best_fit = champ.f;
+				best_ind = i;
+			if(evs == 0):
+				avg_fits[i] = champ.f;
+			else:
+				avg_fits[i] = avg_fits[i] * 0.9 + champ.f[0] * 0.1;
+			print 'avg_fit %3.3f' % avg_fits[i],
+			
+			# only numbers with spaces so that we can easily import them automatically afterwards:
+			#f.write('x = array([')
+			j = 1
+			for ss in champ.x:
+				f.write("".join(str(ss)))
+				if j < len(champ.x):
+					f.write(' ') #(',')
+				j = j + 1
+			#f.write('])')
+			f.write('\n')
+			f2.write("".join(str(champ.f)))
+			f2.write('\n')
+			i += 1
+		
+			f.close()
+			f2.close()
+		print ' '
+
+	# return the best individual:
+	genome = archi[best_ind].population.champion.x;
+	(Rs, Ts, X) = prob.transformGenomeToMatrices(genome, n_views, n_points);
+	return (Rs, Ts, X);
+	
--- code/py2/reconstructionProblem.py
+++ code/py2/reconstructionProblem.py
@@ -1,25 +1,120 @@
 from PyGMO import *
+from PyGMO.problem import base
 from guidobox import *
-from VisualOdometry import *
+import VisualOdometry as VO
 
-class reconstructionProblem extends problem
+class reconstructionProblem(base):
 
-	def obj_fun_impl(genome):
+	n_cameras = 2;
+	n_world_points = 100;
+	t_scale = 3.0;
+	world_scale = 10.0;
+	K = [];
+	IPs = [];
+	genome_size = 0;
+
+	def __init__(self, n_views=2, n_points=100, IPs=[], t_scale = 3.0, world_scale=10.0, K = []):
+		# per view, there are 6 parameters: phi, theta, psi, t1, t2, t3
+		# per point, there are 3 parameters: X, Y, Z
+		self.genome_size = (n_views-1) * 6 + n_points * 3;
+		self.n_cameras = n_views;
+		self.n_world_points = n_points;
+		self.IPs = IPs; # IPs contains per camera the image points
+		self.K = K;
+		self.t_scale = t_scale;
+		self.world_scale = world_scale;
+		super(reconstructionProblem,self).__init__(self.genome_size);
+		
+	def getGenomeSize(self):
+		return self.genome_size;
+
+	def setK(self,K):
+		self.K = K;
+
+	def _objfun_impl(self,genome):
 
 		# the image points are necessary for the fitness evaluation:
-		params = np.loadtxt('params.dat');
-		n_cameras = params[0];
-		n_world_points = params[1];
-		IPs1 = np.loadtxt('IPs1.dat');
-		IPs2 = np.loadtxt('IPs2.dat');
-		IPs = IPs1.append(IPs2[-1]);
-		K = np.loadtxt('K.dat');
+		# params = np.loadtxt('params.dat');
+		# n_cameras = params[0];
+		# n_world_points = params[1];
+		# IPs1 = np.loadtxt('IPs1.dat');
+		# IPs2 = np.loadtxt('IPs2.dat');
+		# IPs = IPs1.append(IPs2[-1]);
+		# K = np.loadtxt('K.dat');
 		
 		# get the rotations, translations, and world points from the genome
-		(Rs, Ts, X) = transformGenomeToMatrices(genome, n_cameras, n_world_points);
+		(Rs, Ts, X) = self.transformGenomeToMatrices(genome, self.n_cameras, self.n_world_points);
 
 		# calculate the reprojection error:
-		err = calculateReprojectionError(Rs, Ts, X, IPs, n_cameras, n_world_points, K);
+		# What if points are behind the cameras?
+		err = VO.calculateReprojectionError(Rs, Ts, X, self.IPs, self.n_cameras, self.n_world_points, self.K);
 
-		return err;
+		# return the fitness:
+		return [float(err)];
 			
+	def transformGenomeToMatrices(self, genome, n_cameras, n_points):
+		""" Decode the genome to the rotation, translation and world point information.
+		"""		
+
+		# decode cameras:
+		Rs = [];
+		Ts = [];
+		# first camera:
+		R = self.getRotationMatrix(0.0, 0.0, 0.0);
+		t = np.zeros([3,1]);
+		Rs.append(R);
+		Ts.append(t);
+
+		for c in range(n_cameras-1):
+			# decode rotation:
+			phi = genome[c*6] * np.pi;
+			theta = genome[c*6+1] * np.pi;
+			psi = genome[c*6+2] * np.pi;
+			R = self.getRotationMatrix(phi, theta, psi);
+			Rs.append(R);
+			# decode translation:
+			t = np.zeros([3,1]);
+			t[0] = genome[c*6+3] * self.t_scale;
+			t[1] = genome[c*6+4] * self.t_scale;
+			t[2] = genome[c*6+5] * self.t_scale;
+			Ts.append(t);
+		
+		# decode world points:
+		offset = (n_cameras-1)*6;
+		X = np.zeros([n_points, 3]);
+		for p in range(n_points):
+			X[p,0] = genome[offset+p*3] * self.world_scale;
+			X[p,1] = genome[offset+p*3+1] * self.world_scale;
+			X[p,2] = genome[offset+p*3+2] * self.world_scale;
+		
+		return (Rs, Ts, X);
+
+		
+
+	def getRotationMatrix(self, phi, theta, psi):
+		""" Create rotation matrix R on the basis of phi, theta, psi 
+		"""
+		R_phi = np.zeros([3,3]);
+		R_phi[0,0] = 1;
+		R_phi[1,1] = np.cos(phi);
+		R_phi[1,2] = np.sin(phi);
+		R_phi[2,1] = -np.sin(phi);
+		R_phi[2,2] = np.cos(phi);
+
+		R_theta = np.zeros([3,3]);
+		R_theta[1,1] = 1;
+		R_theta[0,0] = np.cos(theta);
+		R_theta[2,0] = -np.sin(theta);
+		R_theta[0,2] = np.sin(theta);
+		R_theta[2,2] = np.cos(theta);
+
+		R_psi = np.zeros([3,3]);
+		R_psi[0,0] = np.cos(psi);
+		R_psi[0,1] = np.sin(psi);
+		R_psi[1,0] = -np.sin(psi);
+		R_psi[1,1] = np.cos(psi);
+		R_psi[2,2] = 1;
+
+		R = np.dot(R_psi, np.dot(R_theta, R_phi));
+
+		return R;
